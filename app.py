5d8b950fe0773d3e333bf9518add65706ee30934
Ken Kender <emptyken37@gmail.com>
2025-08-04 16:01:11 +0700

    Fix: bind to 0.0.0.0 and use PORT env

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ def download():
         return "ยังไม่มีข้อมูล Check-in"
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
